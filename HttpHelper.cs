8ecd2847a0174624a9a4fbc742e8c82254c86459
Alexander Scheurer <mail@aspepex.net>
2023-01-08 16:50:27 +0100

    it works ... barely

---
+++ HttpHelper.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+
+namespace VintedImagegrabber
+{
+    internal class HttpHelper
+    {
+        readonly HttpClient httpClient;
+
+        public HttpHelper()
+        {
+            CookieContainer cookies = new();
+            HttpClientHandler handler = new()
+            {
+                CookieContainer = cookies
+            };
+            httpClient = new HttpClient(handler);
+
+            //httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.162 Safari/537.36");
+        }
+
+        public async Task DownloadImageAsync(string directoryPath, string fileName, Uri uri, bool verbose)
+        {
+            // Get the file extension
+            var uriWithoutQuery = uri.GetLeftPart(UriPartial.Path);
+            var fileExtension = Path.GetExtension(uriWithoutQuery);
+
+            // Create file path and ensure directory exists
+            var path = Path.Combine(directoryPath, $"{fileName}{fileExtension}");
+            Directory.CreateDirectory(directoryPath);
+
+            if (File.Exists(path))
+            {
+                if (verbose)
+                    Console.WriteLine($"Skipping {fileName}");
+                return;
+            }
+
+            Console.WriteLine($"DL {fileName}");
+            // Download the image and write to the file
+            var imageBytes = await httpClient.GetByteArrayAsync(uri);
+
+            await File.WriteAllBytesAsync(path, imageBytes);
+        }
+
+        public async Task<string> GetUrl(string url)
+        {
+            return await httpClient.GetStringAsync(url);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿using CommandLine;
+using CommunityToolkit.Diagnostics;
+using Ical.Net;
+using Ical.Net.CalendarComponents;
+using Ical.Net.DataTypes;
+using Ical.Net.Serialization;
+using System.Text.RegularExpressions;
+using System.Web;
+using VintedImagegrabber;
+
+namespace GdqScheduleToIcal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var gsti = new GSTI(args);
+            gsti.Run();
+        }
+
+        class GSTI
+        {
+            public class Options
+            {
+                [Option('u', "scheduleurl", Required = true, HelpText = "GDQ Schedule URL")]
+                public string? ScheduleUrl { get; set; }
+            }
+
+            readonly ParserResult<Options> options;
+            readonly HttpHelper httpHelper;
+
+            public GSTI(string[] args)
+            {
+                options = Parser.Default.ParseArguments<Options>(args);
+                httpHelper = new HttpHelper();
+            }
+
+            public void Run()
+            {
+                options?.WithParsed(o =>
+                {
+                    if (!string.IsNullOrWhiteSpace(o.ScheduleUrl))
+                    {
+                        var schedulePageSource = HttpUtility.HtmlDecode(httpHelper.GetUrl(o.ScheduleUrl).Result);
+                        //var schedulePageSource = HttpUtility.HtmlDecode(File.ReadAllText("schedulePageSource.html"));
+
+                        schedulePageSource = Regex.Replace(schedulePageSource, @"^\s", @"^");
+                        schedulePageSource = Regex.Replace(schedulePageSource, @"\s$", @"$");
+
+                        var calendar = ParseSchedulePage(schedulePageSource);
+
+                        calendar.Name = "AGDQ 2023";
+
+                        var serializer = new CalendarSerializer();
+                        var serializedCalendar = serializer.SerializeToString(calendar);
+
+                        Directory.CreateDirectory("../../../docs");
+                        File.WriteAllText("../../../docs/agdq2023.ical", serializedCalendar);
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR: 1");
+                    }
+                });
+            }
+
+            private Calendar ParseSchedulePage(string schedulePageSource)
+            {
+                // snipping out the whole schedule using <tbody>
+
+                string patternSchedule = @"<tbody>(.*?)</tbody>";
+
+                MatchCollection matchListSchedule = Regex.Matches(schedulePageSource, patternSchedule, RegexOptions.Singleline);
+
+                var calendar = new Calendar();
+
+                if (matchListSchedule.Count > 0 && matchListSchedule[0].Groups.Count > 1)
+                {
+                    var scheduleSource = matchListSchedule[0].Groups[1].Value;
+
+                    string patternEvent = @"<tr>\s*?<td class=""start-time text-right"">(.*?)</td>\s*?<td>(.*?)</td>\s*?<td>(.*?)</td>\s*?<td rowspan=""2"" class=""visible-lg text-center"">.*?(\d{1,2}:\d{2}:\d{2}).*?</td>\s*?</tr>\s*?<tr class=""second-row "">\s*?<td class=""text-right "">.*?(\d{1,2}:\d{2}:\d{2}).*?</td>\s*?<td>(.*?)</td>\s*?<td><i class=""fa fa-microphone""></i> (.*?)</td>";
+
+                    MatchCollection matchListEvents = Regex.Matches(scheduleSource, patternEvent, RegexOptions.Singleline, new TimeSpan(0, 1, 0));
+
+                    foreach (Match match in matchListEvents)
+                    {
+                        if (match != null && match.Groups.Count == 8)
+                        {
+                            var dtStart = new CalDateTime(DateTime.Parse(match.Groups[1].Value).ToUniversalTime());
+                            var game = match.Groups[2].Value;
+                            var runner = match.Groups[3].Value;
+                            var setupRaw = match.Groups[4].Value;
+                            var durationRaw = match.Groups[5].Value.ToString().Split(':');
+                            var rundesc = match.Groups[6].Value;
+                            var host = match.Groups[7].Value;
+
+                            if (durationRaw.Length != 3)
+                            { ThrowHelper.ThrowArgumentOutOfRangeException(); }
+
+                            var dtEnd = new CalDateTime(dtStart).AddHours(int.Parse(durationRaw[0])).AddMinutes(int.Parse(durationRaw[1])).AddSeconds(int.Parse(durationRaw[2]));
+
+                            var run = new CalendarEvent
+                            {
+                                Start = dtStart,
+                                End = dtEnd,
+                                Summary = game,
+                                Location = @"https://www.twitch.tv/gamesdonequick",
+                                Description = $"Runner: {runner}\nRun: {rundesc}\nHost: {host}\nSetup Lenght: {setupRaw}"
+                            };
+
+                            //if (run.Summary == "Jak II")
+                            //{ }
+
+                            calendar.Events.Add(run);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Warning: match is null or doesn't have all arguments!");
+                        }
+                    }
+                }
+
+                return calendar;
+            }
+        }
+    }
+}
